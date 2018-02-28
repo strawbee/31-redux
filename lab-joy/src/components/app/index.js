39d699fbcdf692405e5bd8cb4a0ca5094b4a36ad
strawbee <yuejiao.hou@gmail.com>
2018-02-28 10:13:58 -0800

    lab complete

--- lab-joy/src/components/app/index.js
+++ lab-joy/src/components/app/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import createStore from '../lib/store';
-import Dashboard from './dashboard/dashboard';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import createStore from '../../lib/store';
+import Dashboard from '../dashboard/dashboard';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 const store = createStore();
 
 class App extends React.Component {
   componentDidMount() {
-    store.subscribe(() => console.log('__STATE__:', store.getState()));
+    store.subscribe(() => console.log('__STATE__: ', store.getState()));
   }
 
   render() {
--- lab-joy/src/components/category/category-form/category-form.js
+++ lab-joy/src/components/category/category-form/category-form.js
@@ -3,36 +3,46 @@ import React from 'react';
 class CategoryForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = this.props.category
-      ? this.props.category
-      : {
-        title: '',
+    this.state = this.props.category ? 
+      this.props.category :
+      { 
+        name: '',
+        budget: 0,
+        editing: false,
       };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    let memberFunctions = Object.getOwnPropertyNames(CategoryForm.prototype);
+    for (let functionName of memberFunctions) {
+      if (functionName.startsWith('handle')) {
+        this[functionName] = this[functionName].bind(this);
+      }
+    }
   }
 
   handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState ({[e.target.name]: e.target.value});
   }
 
   handleSubmit(e) {
     e.preventDefault();
     this.props.onComplete(this.state);
+
+    this.setState({
+      name: '',
+      budget: 0,
+      editing: false,
+    });
   }
 
   render() {
     return (
       <form className="category-form" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          value={this.state.title}
-          onChange={this.handleChange} />
+        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+
+        <input type="number" name="budget" value={this.state.budget} onChange={this.handleChange} />
 
         <button type="submit">{this.props.buttonText}</button>
-      </form>
+      </form>    
     );
   }
 }
